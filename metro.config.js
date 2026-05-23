739aede07e91a4805caa33341dac0d316839be28
mubashirahmadsiddiqui7 <mubashirahmadsiddiqui7@gmail.com>
2026-05-24 01:55:16 +0500

    first commit

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,11 @@
+if (!Array.prototype.toReversed) {
+  Array.prototype.toReversed = function () {
+    return [...this].reverse();
+  };
+}
+
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+
 
 const defaultConfig = getDefaultConfig(__dirname);
 const { assetExts, sourceExts } = defaultConfig.resolver;
